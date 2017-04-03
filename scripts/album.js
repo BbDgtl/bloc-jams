4c932e4837bff9c4e579bcdfc44a237675d8069e
Bobby Spencer <bbdgtl@gmail.com>
2017-04-03 12:49:00 +0200

    complete checkpoint 26 Part 2

--- scripts/album.js
+++ scripts/album.js
@@ -146,11 +146,37 @@
      }
  };
 
+
+ var clickHandler = function (targetElement) {
+
+     var songItem = getSongItem(targetElement);
+
+     //Create a conditional that checks if currently playing song is null.
+     if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+
+ };
+
  var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
  var songRows = document.getElementsByClassName('album-view-song-item');
 
  // Album button templates
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ // Create a variable to store the template for the pause button.
+ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ // Store state of playing songs.
+ var currentlyPlayingSong = null;
 
  window.onload = function () {
      setCurrentAlbum(albumPicasso);
@@ -163,6 +189,11 @@
          if (e.target.parentElement.className === 'album-view-song-item') {
              // Change the content from the number to the play button's HTML.
              e.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+             var songItem = getSongItem(e.target);
+
+             if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                 songItem.innerHTML = playButtonTemplate;
+             }
          }
      });
 
@@ -170,7 +201,19 @@
          songRows[i].addEventListener('mouseleave', function (e) {
              // Revert the content back to the number.
              // Selects first child element, which is the song-item-number element.
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+             //this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+             // #1
+             var songItem = getSongItem(e.target);
+             var songItemNumber = songItem.getAttribute('data-song-number');
+             // #2
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
+         });
+
+         songRows[i].addEventListener('click', function (e) {
+             // Event handler call.
+             clickHandler(e.target);
          });
      }
 
@@ -184,4 +227,32 @@
              i = 0;
          }
      });
+ };
+
+ var findParentByClassName = function (e, targetClass) {
+     if (e) {
+         var currentParent = e.parentElement;
+         while (currentParent.className !== targetClass && currentParent.className !== null) {
+             currentParent = currentParent.parentElement;
+         }
+         return currentParent;
+     }
+ };
+
+ var getSongItem = function (e) {
+     switch (e.className) {
+     case 'album-song-button':
+     case 'ion-play':
+     case 'ion-pause':
+         return findParentByClassName(e, 'song-item-number');
+     case 'album-view-song-item':
+         return element.querySelector('.song-item-number');
+     case 'song-item-title':
+     case 'song-item-duration':
+         return findParentByClassName(e, 'album-view-song-item').querySelector('.song-item-numer');
+     case 'song-item-number':
+         return e;
+     default:
+         return;
+     }
  };
