4a086db37a7e1a5fc9d04c301054ef7e072a1d63
Bobby Spencer <bbdgtl@gmail.com>
2017-04-20 12:48:49 +0200

    complete checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,31 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function (point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-
-var animatePoints = function (points) {
-    forEach(points, revealPoint);
-
-};
-
-window.onload = function () {
-
-    //Automatically animate the points on a tall screen where scrolling can't trigger the animation.
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+// ****** CHECKPOINT 29 ****** Refactor window.onload with the jQuery selector.
+$(window).load(function () {
+    //#1
+    if ($(window).height() > 950) {
+        animatePoints();
     }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    //#2
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    window.addEventListener('scroll', function (event) {
-        //console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    //#3
+    $(window).scroll(function (event) {
+        //#4
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
+});
+
+//#5
+var animatePoints = function () {
+    var revealPoint = function () {
+        //#7
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    //#6
+    $.each($('.point'), revealPoint);
 };
