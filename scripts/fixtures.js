98710993a59e5041422d31b55534f2a18f23d5f5
Bobby Spencer <bbdgtl@gmail.com>
2017-04-20 21:34:05 +0200

    complete checkpoint 32

---
+++ scripts/fixtures.js
@@ -0,0 +1,119 @@
+//*****************  EXAMPLE ALBUMS **********************// 
+
+// Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        {
+            title: 'Blue',
+            duration: '4:26'
+         },
+        {
+            title: 'Green',
+            duration: '3:14'
+         },
+        {
+            title: 'Red',
+            duration: '5:01'
+         },
+        {
+            title: 'Pink',
+            duration: '3:21'
+         },
+        {
+            title: 'Magenta',
+            duration: '2:15'
+         }
+     ]
+};
+
+// Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {
+            title: 'Hello, Operator?',
+            duration: '1:01'
+         },
+        {
+            title: 'Ring, ring, ring',
+            duration: '5:01'
+         },
+        {
+            title: 'Fits in your pocket',
+            duration: '3:21'
+         },
+        {
+            title: 'Can you hear me now?',
+            duration: '3:14'
+         },
+        {
+            title: 'Wrong phone number',
+            duration: '2:15'
+         }
+     ]
+};
+
+// Create a third album object in album.js. Populate the object with the same properties as the other two album objects, but provide values of your choosing.
+var albumMadsen = {
+    title: 'Goodbye Logik',
+    artist: 'Madsen',
+    label: 'solo',
+    year: '2006',
+    albumArtUrl: 'assets/images/album_covers/madsen.jpg',
+    songs: [
+        {
+            title: 'Du Schreibst Geschichte',
+            duration: '3:11'
+        },
+        {
+            title: 'Ein Sturm',
+            duration: '3:50'
+        },
+        {
+            title: 'Piraten',
+            duration: '3:49'
+        },
+        {
+            title: 'Good Bye Logik',
+            duration: '3:44'
+        },
+        {
+            title: 'Ich Rette Die Welt',
+            duration: '3:23'
+        },
+        {
+            title: 'Unzerbrechlich',
+            duration: '3:40'
+        },
+        {
+            title: 'Ich Komme Nicht Mit',
+            duration: '3:02'
+        },
+        {
+            title: 'Der Moment',
+            duration: '4:15'
+        },
+        {
+            title: 'Happy End',
+            duration: '3:47'
+        },
+        {
+            title: 'Ein Produkt',
+            duration: '3:49'
+        },
+        {
+            title: 'Euphorie',
+            duration: '3:28'
+        }
+    ]
+};
+// ******************************  END EXAMPLE ALBUMS *******************//
